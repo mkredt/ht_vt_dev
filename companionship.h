100d799b58caa84e5ac666a7741d52ffcce48910
Michael Kredt <floatsquare@gmail.com>
2017-05-25 17:00:47 -0600

    Actually removed not yet needed '*_builder.h' files, modified comments

--- companionship.h
+++ companionship.h
@@ -16,10 +16,59 @@
 #include <Deque>
 #include <individual.h>
 
-/*
- * LOCAL VARIABLES
- */
-std::string companionship_name;
-std::deque companionship_members;
+class companionship{
+public:
+	/*
+	 * Default <companionship> constructor
+	 */
+	companionship::companionship();
 
+	/*
+	 * Parameterized <companionship> constructor
+	 */
+	companionship::companionship(unsigned int new_companionship_size ,std::string new_companionship_name, std::deque* new_companionship_members);
+
+	/*
+	 * Default <companionship> de-constructor
+	 */
+	companionship::~companionship();
+
+	/*
+	 * Function will change the companionship size; larger in all cases, but only smaller if the appropriate number of companions have been first
+	 * removed
+	 * @param new_companionship_size, an unsigned int which is used to assign companionship_size
+	 */
+	void companionship::setCompanionshipSize(unsigned int new_companionship_size);
+
+	/*
+	 * Function will rename the <companionship>
+	 * @param new_companionship_name, a std::string which is used to assign companionship_name
+	 */
+	void companionship::editCompanionshipName(std::string new_companionship_name);
+
+	/*
+	 * Function will add a new companion to the companionship
+	 * @param new_companion, an <individual*> which is added to companionship_members
+	 */
+	void companionship::addCompanionshipMember(individual* new_companion);
+
+	/*
+	 * Function will remove a companion from the companionship
+	 * @param remove_index, an unsigned int which indicates the index of the companion to remove from companionship_members
+	 */
+	void companionship::removeCompanionshipMember(unsigned int remove_index);
+
+	/*
+	 * Function will return the companionship information including names of <individual> involved
+	 * @return companionship_data, a std::string formatted to include companionship information
+	 */
+	std::string companionship::toString();
+private:
+	/*
+	 * LOCAL VARIABLES
+	 */
+	unsigned int companionship_size;
+	std::string companionship_name;
+	std::deque companionship_members;
+};
 #endif /* COMPANIONSHIP_H_ */
--- household_unit.h
+++ household_unit.h
@@ -18,20 +18,58 @@
 class household_unit{
 public:
 	/*
-	 * FUNCTIONS
+	 * Default <household_unit> constructor
 	 */
 	household_unit::household_unit();
+
+	/*
+	 * Parameterized <household_unit> constructor
+	 * @param new_unit_name, a std::string which will rename the household
+	 * @param new_household_members, a std::deque* which will contain <individual*> to be inserted here
+	 * @param new_household_size, an unsigned int specifying number of potential <individual> who may live at <household_unit>
+	 */
+	household_unit::household_unit(std::string new_unit_name, std::deque* new_household_members, unsigned int new_household_size);
+
+	/*
+	 * Default <household_unit> de-constructor
+	 */
 	household_unit::~household_unit();
-	void household_unit::addMember();
-	void household_unit::removeMember();
-	void household_unit::editHouseholdName();
+
+	/*
+	 * Function will add a member to the <household_unit>
+	 * @param new_member, an <individual*> which will point to the member
+	 */
+	void household_unit::addMember(individual* new_household_member);
+
+	/*
+	 * Function will remove a member from the <household_unit>
+	 * @param member_index, an unsigned int which points to an index of the deque
+	 */
+	void household_unit::removeMember(unsigned int remove_index);
+
+	/*
+	 * Function will change the <household_unit> name
+	 * @param, a std::string
+	 */
+	void household_unit::editHouseholdName(std::string new_household_name);
+
+	/*
+	 * Function will set the size (number of potential inhabitants) of the <household_unit>
+	 * @param new_household_size, an unsigned int which represents the number of inhabitants for the <household_unit>
+	 */
+	void household_unit::editHouesholdSize(unsigned int new_household_size);
+
+	/*
+	 * Function will return the household information and list of names of the inhabitants
+	 * @return household_data, std::string formatted with <household_unit> data and names of the <individual> living there
+	 */
 	std::string household_unit::toString();
 private:
 	/*
 	 * LOCAL VARIABLES
 	 */
+	unsigned int household_size;
 	std::deque unit_members;
 	std::string unit_name;
 };
-
 #endif /* HOUSEHOLD_UNIT_H_ */
