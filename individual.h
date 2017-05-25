4a35a7850d3c1ab8bda9a63e5dd1e0cdfeebbd06
Michael Kredt <floatsquare@gmail.com>
2017-05-25 16:10:14 -0600

    added comments to individual.h

--- individual.h
+++ individual.h
@@ -16,13 +16,46 @@
 class individual{
 public:
 	/*
-	 * FUNCTIONS
+	 * Default <individual> constructor
 	 */
 	individual::individual();
+
+	/*
+	 * Default <individual> de-constructor
+	 */
 	individual::~individual();
-	void individual::setName();
-	void individual::setAge();
+
+	/*
+	 * Function will set name of the <individual>. It will accept
+	 * @param given_name, a std::string which will replace the given_name of the <individual>
+	 */
+	void individual::setName(std::string given_name, std::string surname);
+
+	/*
+	 * Function will set age of the <individual>
+	 * @param new_age, a unsigned int which will replace the age of the <individual>
+	 */
+	void individual::setAge(unsigned int new_age);
+
+	/*
+	 * Function will assign the name of the housing unit in which the <individual> lives
+	 * @param new_household, a std::string which will replace the household name of the <individual>
+	 */
 	void individual::setHoushold();
+
+	/*
+	 * Function will assign the phone number for the <individual>
+	 * @param new_country_code, an unsigned int which will replace the country code of the <individual>
+	 * @param new_area_code, an unsigned int which will replace the area code the <individual>
+	 * @param new_prefix, an unsigned int which will replace the prefix of the <individual>
+	 * @param new_postfix, an unsigned int which will replace the postfix of the <individual>S
+	 */
+	void individual::setPhoneNumber();
+
+	/*
+	 * Function will print the data stored for this <individual>
+	 * @return user_data, a std:string formatted with the <individual> information
+	 */
 	std::string individual::toString();
 private:
 	/*
